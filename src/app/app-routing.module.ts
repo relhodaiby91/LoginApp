2df77de42404af506e94a64a260171c24e84567f
Radwa <radwa.elhodaiby@gizasystems.com>
2023-08-03 16:53:51 +0300

    app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'land', component:LandingPageComponent
+},
+{path:'mewa',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
